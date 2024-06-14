bdf912004a8150404a9fbeffb4976724b8e1732e
Thomas Bechtold <thomas.bechtold@canonical.com>
2024-06-14 11:11:11 +0200

    tests: check for file and deb package in test_config() func test
    Before this commit, the functional testing did run the relevant
    commands but didn't check any content in the resulting chroot. This
    commit does now add very basic file and debian package checks to
    increase test coverage.

--- chimg/tests/functional/test_chroot.py
+++ chimg/tests/functional/test_chroot.py
@@ -1,9 +1,11 @@
 #  SPDX-FileCopyrightText: 2024 Thomas Bechtold <thomasbechtold@jpberlin.de>
 #  SPDX-License-Identifier: GPL-3.0-or-later
 
+import os
 import pathlib
 import pytest
-
+import subprocess
+from functools import partial
 from chimg import chroot
 from chimg import context
 
@@ -11,17 +13,41 @@ from chimg import context
 curdir = pathlib.Path(__file__).parent.resolve()
 
 
+def _check_file_exists(file_path: pathlib.Path, chroot_path: pathlib.Path):
+    """
+    check that a given file exists
+    """
+    assert os.path.exists(chroot_path / file_path)
+
+
+def _check_deb_installed(deb_name: str, chroot_path: pathlib.Path):
+    """
+    check that a given deb package is installed
+    """
+    res = subprocess.run(["/usr/sbin/chroot", chroot_path.as_posix(), "dpkg-query", "-W", deb_name])
+    assert res.returncode == 0, f"deb package {deb_name} is not installed"
+
+
 @pytest.mark.parametrize(
-    "config_path",
+    "config_path,checks",
     [
-        "configs/kernel-only.yaml",
+        [
+            "configs/kernel-only.yaml",
+            [
+                (partial(_check_file_exists, pathlib.Path("/boot/vmlinuz"))),
+                (partial(_check_deb_installed, "linux-aws")),
+            ],
+        ],
     ],
 )
 @pytest.mark.realchroot
-def test_config(chroot_mmdebstrap_dir, config_path):
+def test_config(chroot_mmdebstrap_dir, config_path, checks):
     """
     Test different configuration examples from the functional/configs directory
     """
     ctx = context.Context(conf_path=curdir / config_path, chroot_path=chroot_mmdebstrap_dir)
     cr = chroot.Chroot(ctx)
     cr.apply()
+    # do checks
+    for check in checks:
+        check(chroot_mmdebstrap_dir)
